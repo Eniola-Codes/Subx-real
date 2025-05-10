f63a0f2a697c8ce6b34caae1e1a15588b0ab8319
Eniola Odunmbaku <eniolacodes1@gmail.com>
2025-05-10 20:47:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 5173,
     strictPort: true,
@@ -11,5 +12,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
 })
